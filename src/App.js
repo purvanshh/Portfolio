706d08bd40cff29292ee9d6bf20894f35f24edb0
Purvansh <purvansh.23bcs10169@ms.sst.scaler.com>
2024-05-31 11:44:53 +0530

    Added More tiles Changed color of progress bar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import AboutMePage from './All Component/About/AboutMePage.js';
 import './App.css';
 import Component from './All Component/Component/Component.js';
-import Navbar from './All Component/Navbar/navbar.js';
+import Navbar from './All Component/Navbar/Navbar.js';
 import Tile from './All Component/Tile/Tile.js';
 import ProgressBar from './All Component/ProgressBar/ProgressBar.js';
 
@@ -21,15 +21,35 @@ function App() {
     <div className="App">
       <Navbar />
       <Component />
-      <Tile
-        header="Hello"
-        subHeader="World"
-        img="https://imgs.search.brave.com/b20GAw5dHFdFadqg6JqUsOdDmk9-lmNFm_E7cKm3mD8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/bG9nby53aW5lL2Ev/bG9nby9HaXRIdWIv/R2l0SHViLUljb24t/V2hpdGUtRGFyay1C/YWNrZ3JvdW5kLUxv/Z28ud2luZS5zdmc.svg"
-        text="This is a sample text"
-        link="#"
-        linkText="Click me!"
-        position="left"
-      />
+      <div className="tiles-container">
+        <Tile
+          header="Hello"
+          subHeader="World"
+          img="https://imgs.search.brave.com/b20GAw5dHFdFadqg6JqUsOdDmk9-lmNFm_E7cKm3mD8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/bG9nby53aW5lL2Ev/bG9nby9HaXRIdWIv/R2l0SHViLUljb24t/V2hpdGUtRGFyay1C/YWNrZ3JvdW5kLUxv/Z28ud2luZS5zdmc.svg"
+          text="This is a sample text"
+          link="#"
+          linkText="Click me!"
+          position="left"
+        />
+        <Tile
+          header="Welcome"
+          subHeader="Everyone"
+          img="https://imgs.search.brave.com/b20GAw5dHFdFadqg6JqUsOdDmk9-lmNFm_E7cKm3mD8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/bG9nby53aW5lL2Ev/bG9nby9HaXRIdWIv/R2l0SHViLUljb24t/V2hpdGUtRGFyay1C/YWNrZ3JvdW5kLUxv/Z28ud2luZS5zdmc.svg"
+          text="Another sample text"
+          link="#"
+          linkText="Learn more"
+          position="center"
+        />
+        <Tile
+          header="React"
+          subHeader="Components"
+          img="https://imgs.search.brave.com/b20GAw5dHFdFadqg6JqUsOdDmk9-lmNFm_E7cKm3mD8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/bG9nby53aW5lL2Ev/bG9nby9HaXRIdWIv/R2l0SHViLUljb24t/V2hpdGUtRGFyay1C/YWNrZ3JvdW5kLUxv/Z28ud2luZS5zdmc.svg"
+          text="Yet another sample text"
+          link="#"
+          linkText="Discover"
+          position="right"
+        />
+      </div>
       <ProgressBar progress={progress} />
       <AboutMePage />
     </div>
